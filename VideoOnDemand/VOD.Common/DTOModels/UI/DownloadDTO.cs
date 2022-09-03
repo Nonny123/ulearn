909e56023c6369fe70214aebc9cb2354d31cf2a9
Nonso <justcoolk@yahoo.com>
2022-09-03 22:06:51 +0100

    Add UI Dtos

---
+++ VideoOnDemand/VOD.Common/DTOModels/UI/DownloadDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Common.DTOModels.UI
+{
+    public class DownloadDTO
+    {
+        public string DownloadUrl { get; set; }
+        public string DownloadTitle { get; set; }
+    }
+}
---
+++ VideoOnDemand/VOD.Common/DTOModels/UI/InstructorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Common.DTOModels.UI
+{
+    public class InstructorDTO
+    {
+        public string InstructorName { get; set; }
+        public string InstructorDescription { get; set; }
+        public string InstructorAvatar { get; set; }
+    }
+}
