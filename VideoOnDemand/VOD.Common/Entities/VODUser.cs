80fbae6c7a4fa9a9bbb048841cc9abf3a83bcfd4
Chinonso Okafor <tmp11255@firstbanknigeria.com>
2022-10-20 16:59:14 +0100

    remove unsed namespaces

--- VideoOnDemand/VOD.Common/Entities/VODUser.cs
+++ VideoOnDemand/VOD.Common/Entities/VODUser.cs
@@ -2,10 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace VOD.Common.Entities
 {
